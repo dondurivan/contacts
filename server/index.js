de3a9f76ea40274ebe10c9a4c0449a50c17033ce
Ivan Dondur <ivandondur@Ivanas-MacBook-Air.local>
2021-11-15 21:09:31 +0100

    Move server script to server folder

--- server/index.js
+++ server/index.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 require("dotenv").config();
 
 const url = process.env.MONGODB_URL
