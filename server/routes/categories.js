77af57a92b51d95e00fd36aad5321a571a80f689
Ivan Dondur <ivandondur@Ivanas-MacBook-Air.local>
2021-11-23 07:23:08 +0100

    Add delete category route

--- server/routes/categories.js
+++ server/routes/categories.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 let Category = require('../models/category.model')
 
+// Get categories
 router.get('/', async (req, res) => {
     try {
         const categories = await Category.find();
@@ -12,6 +13,7 @@ router.get('/', async (req, res) => {
     }
 })
 
+// Add single category
 router.post('/add', async(req, res) => {
     const { name, image } = req.body;
 
@@ -26,12 +28,29 @@ router.post('/add', async(req, res) => {
     }
 })
 
+// Get single category by id
 router.get('/:id', async (req, res) => {
     try {
         const item = await Category.findById(req.params.id);
         if (!item) throw Error('No category found');
 
         res.status(200).json(item);
+    } catch (e) {
+        res.status(400).json('error: ' + e.message)
+    }
+})
+
+// Delete category
+router.delete('/:id', async(req, res) => {
+    try {
+        const item = await Category.findById(req.params.id);
+        if (!item) throw Error('No category found');
+
+        const removed = await item.remove()
+        if (!removed)
+            throw Error('Something went wrong while trying to delete the item');
+
+        res.status(200).json({ success: true });
     } catch (e) {
         res.status(400).json('error: ' + e.message)
     }
